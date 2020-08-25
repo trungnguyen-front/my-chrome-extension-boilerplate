5ec36a8a3019b7abbeecf759d902e71cda16b595
KennyNguyen88 <giatrangrua@gmail.com>
2020-08-25 22:07:35 +0700

    init based on https://levelup.gitconnected.com/how-to-use-background-script-to-fetch-data-in-chrome-extension-ef9d7f69625d

---
+++ src/helpers.js
@@ -0,0 +1,8 @@
+export const fetchRepositories = async () => {
+  console.log("fetchRepositories");
+  return "fetchRepositories";
+};
+
+export const saveToLocalStorage = () => {
+  console.log("saveToLocalStorage");
+};
